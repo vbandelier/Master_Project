627158d19bb01a34b2c4de319b280ee5f5cc3554
vbandelier <valentin.bandelier@epfl.ch>
2017-03-17 21:50:50 +0100

    GHQC beta

--- matlab/GHQC.m
+++ matlab/GHQC.m
@@ -2,14 +2,53 @@
 clear 
 clc
 %  European Option
- S0 = 100;
- K  = 100;
- sigma = 0.2;
- r = 0;
- q = 0;
- T = 1;
+S0 = 100;
+K  = 100;
+sigma = 0.2;
+r = 0;
+q = 0;
+T = 1;
  
- % BLS price
- [c,~] = blsprice(S0,K,r,T,sigma,q);
+M = 100;
+N = 1;
  
+% BLS price
+[c,~] = blsprice(S0,K,r,T,sigma,q); 
+ 
+% Step 1
+Smin = S0 * exp(-5*sigma*sqrt(T));
+Smax = S0 * exp(5*sigma*sqrt(T));
+
+Xmin = log(Smin/S0);
+Xmax = log(Smax/S0);
+
+h = (Xmax-Xmin)/M;
+X = Xmin + (0:M)*h;
+S = S0*exp(X);
+dt = T/N; 
+t = (0:N)*dt;
+ 
+tau = (r-q-0.5*sigma^2)*dt;
+nu = sigma*sqrt(dt);
+ 
+% Step 2
+Q = max(S-K,0);
+
+for k = 1:N
+% Step 3
+Qint = griddedInterpolant(X,Q,'spline');
+ 
+% Step 4
+xi = [-2.35061,  -1.33585, -0.436077, 0.436077, 1.33585,  2.35061];
+w  = [0.00453001, 0.157067, 0.72463,  0.72463,  0.157067, 0.00453001];
+
+Qnew = zeros(size(Q));
+for m = 1:M+1
+    Qnew(m) = exp(-r*dt)/sqrt(pi) * (w*Qint(sqrt(2)*tau*xi+nu+X(m))');
+end
+ 
+Q = Qnew;
+end
+
+price = interp1(S,Q,S0);
  
