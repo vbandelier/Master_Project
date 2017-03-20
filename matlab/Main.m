4f2707eacb0bfb61237bc1f5987bc34fef9f345c
vbandelier <valentin.bandelier@epfl.ch>
2017-03-20 08:17:02 +0100

    20.03.2017

--- matlab/Main.m
+++ matlab/Main.m
@@ -33,9 +33,11 @@ for i = 1:4
     Targ = Target(i);
     for j = 1:3
         KO = KO_type(j,:);
+        tic
         Prices_MC(i,j) = MCTarnPricing(S_0,K,r_d,r_f,sigma,Period,Targ,N_fixDates,N_sim,gainFun,KO);
         Prices_FD(i,j) = FDTarnPricing(S_0,K,r_d,r_f,sigma,Period,Targ,N_fixDates,Nx,Nt,Na,KO,theta);
         Prices_GHQC(i,j) = GHQCTarnPricing(S_0,K,r_d,r_f,sigma,Period,Targ,N_fixDates,Nx,Na,KO,q_order);
+        toc
     end
 end
 %% Results
