e43ed98945887101555ca9b5a2ec9b8c34a3e835
vbandelier <valentin.bandelier@epfl.ch>
2017-05-09 21:22:38 +0200

    09.05.2017

--- matlab/Final code/Functions/Price.m
+++ matlab/Final code/Functions/Price.m
@@ -1,4 +1,5 @@
 function p = Price(option,model,method)
+%% Option Parameters
 N = option.N_fixDates;
 g = option.g;
 gain_fun = option.gain_fun;
@@ -6,40 +7,41 @@ loss_fun = option.loss_fun;
 Targ = option.Target;
 K = option.K;
 r = option.r;
+%% Monte Carlo Method -----------------------------------------------------
 if method.name(1) == 'M'
     M = method.param(1);
     period = option.T/option.N_fixDates;
     if model.name(1) == 'B'
         sigma = model.param(1);
-        S = BS_generator(option.S0,option.r,option.q,option.T,option.N_fixDates,...
-            sigma,M);
+        S = BS_generator(option.S0,option.r,option.q,option.T,...
+            option.N_fixDates,sigma,M);
     elseif model.name(1) == 'M'
         sigma = model.param(1);
         lambda = model.param(2);
         alpha = model.param(3);
         delta = model.param(4);
-        S = Merton_generator(option.S0,option.r,option.q,option.T,option.N_fixDates,...
-            sigma,lambda,alpha,delta,M);
+        S = Merton_generator(option.S0,option.r,option.q,option.T,...
+            option.N_fixDates,sigma,lambda,alpha,delta,M);
     elseif model.name(1) == 'K'
         sigma = model.param(1);
         lambda = model.param(2);
         p = model.param(3);
         eta1 = model.param(4);
         eta2 = model.param(5);
-        S = Kou_generator(option.S0,option.r,option.q,option.T,option.N_fixDates,...
-            sigma,lambda,p,eta1,eta2,M);
+        S = Kou_generator(option.S0,option.r,option.q,option.T,...
+            option.N_fixDates,sigma,lambda,p,eta1,eta2,M);
     elseif model.name(1) == 'N'
         alpha = model.param(1);
         beta = model.param(2);
         delta = model.param(3);
-        S = NIG_generator(option.S0,option.r,option.q,option.T,option.N_fixDates,...
-            alpha,beta,delta,M);
+        S = NIG_generator(option.S0,option.r,option.q,option.T,...
+            option.N_fixDates,alpha,beta,delta,M);
     elseif model.name(1) == 'V'
         theta = model.param(1);
         sigma = model.param(2);
         nu = model.param(3);
-        S = VG_generator(option.S0,option.r,option.q,option.T,option.N_fixDates,...
-            theta,sigma,nu,M);
+        S = VG_generator(option.S0,option.r,option.q,option.T,...
+            option.N_fixDates,theta,sigma,nu,M);
     end
     V = zeros(M,1);
     for i = 1:M
@@ -67,6 +69,10 @@ if method.name(1) == 'M'
         end
     end
     p = mean(V);
+end
+%% Finite Difference Method
+
+%% Convolution Method
 end
 
 
