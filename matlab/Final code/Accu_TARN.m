39cebe54137ffc73b8cfa6fee39253737ac2efea
vbandelier <valentin.bandelier@epfl.ch>
2017-06-21 21:19:24 +0200

    21.06.2017

--- matlab/Final code/Accu_TARN.m
+++ matlab/Final code/Accu_TARN.m
@@ -14,21 +14,21 @@ S0  = 0.9730;
 K   = [0.9275*ones(1,2) 0.935*ones(1,4) 0.942*ones(1,46)];
 r   = -0.01237;
 q   =  0.01197;
-F_fixDates = 6;
-Period = 1/6;
+F_fixDates = 52;
+Period = 1/52;
 T = Period*F_fixDates;
-gaiF_fun = @(S,K) max(S-K,0);
+gain_fun = @(S,K) max(S-K,0);
 loss_fun = @(S,K) max(K-S,0);
 g = 2;
 Targ = 0.4;
 KO = 'F';
-ApF = 1;
+ApF = 40000;
 %% Models
 %  Black-Scholes
 sigma = 0.07908; %bid = 0.07636; mid = 0.7908; ask = 0.08181
 param_bs = sigma;
 model1 = Model('BS',param_bs);
-% mid_NORM2_BS = NORM2(model1.param,model1.name,S0,Data,1000);
+mid_NORM2_BS = NORM2(model1.param,model1.name,S0,Data,1000);
 %%  Merton
 param_mer = [0.0649    0.1303   -0.0584    0.1603]; % Calibrated
 %% Calibration %
@@ -42,7 +42,7 @@ param_mer = [0.0649    0.1303   -0.0584    0.1603]; % Calibrated
 % [param_mer,NORM2_mer] = run(gs,problem);
 %%
 model2 = Model('Mer',param_mer);
-% mid_NORM2_Mer = NORM2(model2.param,model2.name,S0,Data,1000);
+mid_NORM2_Mer = NORM2(model2.param,model2.name,S0,Data,1000);
 %%  Kou
 param_kou = [0.0665    0.1305    0.0751    3.3154    9.0490]; %Calibrated
 %% Calibration %
@@ -56,7 +56,7 @@ param_kou = [0.0665    0.1305    0.0751    3.3154    9.0490]; %Calibrated
 % [param_kou,NORM2_kou] = run(gs,problem);
 %%
 model3 = Model('Kou',param_kou);
-% mid_NORM2_Kou = NORM2(model3.param,model3.name,S0,Data,1000);
+mid_NORM2_Kou = NORM2(model3.param,model3.name,S0,Data,1000);
 %%  NIG
 param_nig = [18.8491   -3.9281    0.1250]; % Calibrated
 %% Calibration %
@@ -70,7 +70,7 @@ param_nig = [18.8491   -3.9281    0.1250]; % Calibrated
 % [param_nig,NORM2_nig] = run(gs,problem);
 %%
 model4 = Model('NIG',param_nig);
-% mid_NORM2_NIG = NORM2(model4.param,model4.name,S0,Data,1000);
+mid_NORM2_NIG = NORM2(model4.param,model4.name,S0,Data,1000);
 %%  VG
 param_vg = [-0.0324    0.0810    0.2451]; % Calibrated
 
@@ -85,7 +85,7 @@ param_vg = [-0.0324    0.0810    0.2451]; % Calibrated
 % [param_vg,NORM2_vg] = run(gs,problem);
 %%
 model5 = Model('VG',param_vg);
-% mid_NORM2_VG = NORM2(model5.param,model5.name,S0,Data,1000);
+mid_NORM2_VG = NORM2(model5.param,model5.name,S0,Data,1000);
 
 %% Methods
 %  Monte Carlo
@@ -103,29 +103,29 @@ alpha = 0;
 method3 = Method('Conv',[Na, Nx, alpha]);
 
 %% Pricing
-TARN_MC_BS  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_MC_Mer = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_MC_Kou = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_MC_NIG = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_MC_VG  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
+TARN_MC_BS  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_MC_Mer = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_MC_Kou = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_MC_NIG = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_MC_VG  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 
-TARN_FD_BS  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_FD_Mer = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_FD_Kou = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_FD_NIG = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_FD_VG  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
+TARN_FD_BS  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_FD_Mer = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_FD_Kou = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_FD_NIG = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_FD_VG  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 %
-TARN_Conv_BS  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_Conv_Mer = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_Conv_Kou = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_Conv_NIG = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-TARN_Conv_VG  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
+TARN_Conv_BS  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_Conv_Mer = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_Conv_Kou = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_Conv_NIG = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+TARN_Conv_VG  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 
-Nref_TARN_BS  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-Nref_TARN_Mer  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-Nref_TARN_Kou  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-Nref_TARN_NIG  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
-Nref_TARN_VG  = Option(S0,r,q,K,Period,F_fixDates,gaiF_fun,loss_fun,g,Targ,KO,ApF);
+Nref_TARN_BS  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+Nref_TARN_Mer  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+Nref_TARN_Kou  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+Nref_TARN_NIG  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
+Nref_TARN_VG  = Option(S0,r,q,K,Period,F_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 
 figure
 hold on
--- matlab/Final code/Pivot_TARN.m
+++ matlab/Final code/Pivot_TARN.m
@@ -1,11 +1,9 @@
 clc
 close all
 %% Market data
-% [Prices,Strikes,Maturity,r,q,Option_type] = importfile('EURUSD_20170524_mid.csv',2, 111);
-% Data = [Prices,Strikes,Maturity,r,q,Option_type];
-% options = optimset('display','iter','FunValCheck', 'on', 'MaxFunEvals', 5000, 'MaxIter', 5000);
-% opts = optimoptions(@fmincon,'Algorithm','interior-point');
-% gs = GlobalSearch('Display','iter');
+[Prices,Strikes,Maturity,r,q,Option_type] = importfile('EURUSD_20170524_ask.csv',2, 122);
+Data = [Prices,Strikes,Maturity,r,q,Option_type];
+options = optimset('display','iter','FunValCheck', 'on', 'MaxFunEvals', 5000, 'MaxIter', 5000);
 %% Option Parameters
 S0  = 1.1187;
 K   = 0;
@@ -22,15 +20,15 @@ KO = 'F';
 ApF = 375000;
 %% Models
 %  Black-Scholes
-sigma = 0.07880; %bid =0.07745 ; mid = 0.0788; ask =0.08015 ; 
+sigma = 0.08015; %bid =0.07745 ; mid = 0.0788; ask =0.08015 ; 
 param_bs = sigma;
 model1 = Model('BS',param_bs);
-% NORM2_BS = NORM2(model1.param,model1.name,S0,Data,1000);
+NORM2_BS = NORM2(model1.param,model1.name,S0,Data,1000);
 %%  Merton
 param_mer = [0.0669    0.1040   -0.0504    0.1650]; % Calibrated
 %% Calibration %
-% Nx = 1000;
-% param_mer = fminsearch(@(p) NORM2(p, 'Mer', S0, Data,Nx),param_mer,options);
+Nx = 1000;
+param_mer = fminsearch(@(p) NORM2(p, 'Mer', S0, Data,Nx),param_mer,options);
 % lb = [0 0 -Inf 0];
 % ub = [Inf Inf Inf Inf];
 % param_mer = fmincon(@(p) NORM2(p, 'Mer', S0, Data,Nx),param_mer,[],[],[],[],lb,ub,[],options);
@@ -39,12 +37,12 @@ param_mer = [0.0669    0.1040   -0.0504    0.1650]; % Calibrated
 % [param_mer,NORM2_mer] = run(gs,problem);
 %%
 model2 = Model('Mer',param_mer);
-% NORM2_Mer = NORM2(model2.param,model2.name,S0,Data,1000);
+NORM2_Mer = NORM2(model2.param,model2.name,S0,Data,1000);
 %%  Kou
 param_kou = [0.0657    0.2065    0.5726   16.0440    8.3150]; %0Calibrated
 %% Calibration %
-% Nx = 1000;
-% param_kou = fminsearch(@(p) NORM2(p, 'Kou', S0, Data,Nx),param_kou,options);
+Nx = 1000;
+param_kou = fminsearch(@(p) NORM2(p, 'Kou', S0, Data,Nx),param_kou,options);
 % lb = [0 0 0 0 0];
 % ub = [Inf Inf 1 Inf Inf];
 % param_kou = fmincon(@(p) NORM2(p, 'Kou', S0, Data,Nx),param_kou,[],[],[],[],lb,ub,[],options);
@@ -53,12 +51,12 @@ param_kou = [0.0657    0.2065    0.5726   16.0440    8.3150]; %0Calibrated
 % [param_kou,NORM2_kou] = run(gs,problem);
 %%
 model3 = Model('Kou',param_kou);
-% NORM2_Kou = NORM2(model3.param,model3.name,S0,Data,1000);
+NORM2_Kou = NORM2(model3.param,model3.name,S0,Data,1000);
 %%  NIG
 param_nig = [23.0248   -3.0954    0.1500]; % Calibrated
 %% Calibration %
-% Nx = 1000;
-% param_nig = fminsearch(@(p) NORM2(p, 'NIG', S0, Data,Nx),param_nig,options);
+Nx = 1000;
+param_nig = fminsearch(@(p) NORM2(p, 'NIG', S0, Data,Nx),param_nig,options);
 % lb = [0  -Inf  0];
 % ub = [Inf Inf Inf];
 % param_nig = fmincon(@(p) NORM2(p, 'NIG', S0, Data,Nx),param_nig,[],[],[],[],lb,ub,[],options);
@@ -67,13 +65,13 @@ param_nig = [23.0248   -3.0954    0.1500]; % Calibrated
 % [param_nig,NORM2_nig] = run(gs,problem);
 %%
 model4 = Model('NIG',param_nig);
-% NORM2_NIG = NORM2(model4.param,model4.name,S0,Data,1000);
+NORM2_NIG = NORM2(model4.param,model4.name,S0,Data,1000);
 %%  VG
 param_vg = [-0.0235    0.0802    0.1658]; % Calibrated
 
 %% Calibration %
-% Nx = 1000;
-% param_vg = fminsearch(@(p) NORM2(p, 'VG', S0, Data,Nx),param_vg,options);
+Nx = 1000;
+param_vg = fminsearch(@(p) NORM2(p, 'VG', S0, Data,Nx),param_vg,options);
 % lb = [-Inf  0  0];
 % ub = [Inf Inf Inf];
 % param_vg = fmincon(@(p) NORM2(p, 'VG', S0, Data,Nx),param_vg,[],[],[],[],lb,ub,[],options);
@@ -82,7 +80,7 @@ param_vg = [-0.0235    0.0802    0.1658]; % Calibrated
 % [param_vg,NORM2_vg] = run(gs,problem);
 %%
 model5 = Model('VG',param_vg);
-% NORM2_VG = NORM2(model5.param,model5.name,S0,Data,1000);
+NORM2_VG = NORM2(model5.param,model5.name,S0,Data,1000);
 
 %% Methods
 %  Monte Carlo
@@ -118,8 +116,8 @@ TARN_Conv_Kou = Option(S0,r,q,K,Period,N_fixDates,gain_fun,loss_fun,g,Targ,KO,Ap
 TARN_Conv_NIG = Option(S0,r,q,K,Period,N_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 TARN_Conv_VG  = Option(S0,r,q,K,Period,N_fixDates,gain_fun,loss_fun,g,Targ,KO,ApF);
 
-figure
-hold on
+% figure
+% hold on
 
 disp('---------- Black-Scholes ----------')
 % TARN_MC_BS.set_price(model1,method1);
